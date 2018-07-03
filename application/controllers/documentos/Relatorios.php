b19892038a55fd9a33ec3419b9d17d15fe8847b7
Joyce Carvalho <joycegcarvalho@hotmail.com>
2018-07-03 15:41:43 -0300

    Alteração listagem de documentos em andamento

--- application/controllers/documentos/Relatorios.php
+++ application/controllers/documentos/Relatorios.php
@@ -23,11 +23,16 @@ class Relatorios extends CI_Controller {
         $dados["submenu"] = "andamento";
 
         $dados["nome_empresa"]  = $this->empresamodel->nome_empresa($_SESSION["idempresa"]);
+
         if (($_SESSION["is_admin"] == true) or ($_SESSION["is_coordenador"] == true)) {
+
             $dados["andamento_doc_c"] = $this->docmodel->listar_documentos_em_andamento($_SESSION["idempresa"]);
+
         } else {
+
             $dados["andamento_doc_f"] = $this->docmodel->listar_documentos_andamento_cargos($_SESSION["idusuario"]);
             $dados["andamento_doc_c"] = $this->docmodel->listar_documentos_andamento_funcionarios($_SESSION["idusuario"]);
+            
         }
 
         $this->load->view("template/html_header", $dados);
