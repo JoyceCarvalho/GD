171c3af47e28c057a5135810e3c08409648f3556
Joyce Carvalho <joycegcarvalho@hotmail.com>
2018-07-06 13:42:32 -0300

    Atualização do sql do banco

--- db/banco_gd.sql
+++ db/banco_gd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 25-Jun-2018 às 19:19
+-- Generation Time: 06-Jul-2018 às 18:23
 -- Versão do servidor: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -25,33 +25,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbausencia`
+-- Estrutura da tabela `tbempresa`
 --
 
-CREATE TABLE `tbausencia` (
+CREATE TABLE `tbempresa` (
   `id` int(11) NOT NULL,
-  `dia_inicio` datetime DEFAULT NULL,
-  `dia_fim` datetime DEFAULT NULL,
-  `motivo` text,
-  `fk_idusuario` int(11) NOT NULL,
-  `fk_idempresa` int(11) DEFAULT NULL
+  `nome` varchar(255) DEFAULT NULL,
+  `cliente_code` varchar(45) DEFAULT NULL,
+  `logo_code` varchar(45) DEFAULT NULL,
+  `missao` varchar(255) DEFAULT NULL,
+  `visao` varchar(255) DEFAULT NULL,
+  `valores` text,
+  `ativo` int(1) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `tbempresa`
+--
+
+INSERT INTO `tbempresa` (`id`, `nome`, `cliente_code`, `logo_code`, `missao`, `visao`, `valores`, `ativo`) VALUES
+(1, 'SGT - Gestão e Tecnologia', 'sgtgestaoetecnologia', NULL, 'Proporcionar ferramentas que garantam o acesso a melhores práticas de gestão organizacional e de qualidade', NULL, 'Respeito ao usuário.\r\nRapidez nas soluções.\r\nInovação permantente.', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbcancelamento`
+-- Estrutura da tabela `tbhorario_trab`
 --
 
-CREATE TABLE `tbcancelamento` (
+CREATE TABLE `tbhorario_trab` (
   `id` int(11) NOT NULL,
-  `motivo` varchar(255) DEFAULT NULL,
-  `data_hora` datetime DEFAULT NULL,
-  `fk_iddocumento` int(11) NOT NULL,
-  `fk_idusuario` int(11) NOT NULL
+  `titulo` varchar(255) DEFAULT NULL,
+  `manha_entrada` time DEFAULT NULL,
+  `manha_saida` time DEFAULT NULL,
+  `tarde_entrada` time DEFAULT NULL,
+  `tarde_saida` time DEFAULT NULL,
+  `fk_idempresa` int(11) DEFAULT NULL,
+  `ativo` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `tbhorario_trab`
+--
+
+INSERT INTO `tbhorario_trab` (`id`, `titulo`, `manha_entrada`, `manha_saida`, `tarde_entrada`, `tarde_saida`, `fk_idempresa`, `ativo`) VALUES
+(1, 'Regular', '07:00:00', '12:00:00', '13:00:00', '18:00:00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -70,21 +87,84 @@ CREATE TABLE `tbcargos` (
 --
 
 INSERT INTO `tbcargos` (`id`, `titulo`, `fk_idempresa`) VALUES
-(1, 'Administrador Master', 1);
+(1, 'Administrador', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbcompetencias`
+-- Estrutura da tabela `tbusuario`
 --
 
-CREATE TABLE `tbcompetencias` (
+CREATE TABLE `tbusuario` (
   `id` int(11) NOT NULL,
-  `tipo` varchar(12) DEFAULT NULL,
-  `fk_iddocumento` int(11) DEFAULT NULL,
-  `fk_idetapa` int(11) DEFAULT NULL,
-  `fk_idusuario` int(11) DEFAULT NULL,
-  `fk_idcargo` int(11) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `senha` varchar(100) DEFAULT NULL,
+  `ativo` int(11) DEFAULT '1',
+  `fk_idempresa` int(11) NOT NULL,
+  `fk_idcargos` int(11) NOT NULL,
+  `fk_idhorariotrab` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tbusuario`
+--
+
+INSERT INTO `tbusuario` (`id`, `nome`, `email`, `usuario`, `senha`, `ativo`, `fk_idempresa`, `fk_idcargos`, `fk_idhorariotrab`) VALUES
+(1, 'Administrador', 'suporte@sgtgestaoetecnologia.com.br', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbausencia`
+--
+
+CREATE TABLE `tbausencia` (
+  `id` int(11) NOT NULL,
+  `dia_inicio` datetime DEFAULT NULL,
+  `dia_fim` datetime DEFAULT NULL,
+  `motivo` text,
+  `fk_idusuario` int(11) NOT NULL,
+  `fk_idempresa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbcancelamento`
+--
+
+CREATE TABLE `tbcancelamento` (
+  `id` int(11) NOT NULL,
+  `motivo` varchar(255) DEFAULT NULL,
+  `data_hora` datetime DEFAULT NULL,
+  `fk_iddocumento` int(11) NOT NULL,
+  `fk_idusuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbgrupo`
+--
+
+CREATE TABLE `tbgrupo` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `fk_idempresa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbetapa`
+--
+
+CREATE TABLE `tbetapa` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `ativo` int(11) DEFAULT '1',
   `fk_idempresa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -100,7 +180,6 @@ CREATE TABLE `tbdocumento` (
   `fk_idgrupo` int(11) NOT NULL,
   `fk_idempresa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -132,26 +211,23 @@ CREATE TABLE `tbdocumentos_cad` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbempresa`
+-- Estrutura da tabela `tberros_tipo`
 --
 
-CREATE TABLE `tbempresa` (
+CREATE TABLE `tberros_tipo` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) DEFAULT NULL,
-  `cliente_code` varchar(45) DEFAULT NULL,
-  `logo_code` varchar(45) DEFAULT NULL,
-  `missao` varchar(255) DEFAULT NULL,
-  `visao` varchar(255) DEFAULT NULL,
-  `valores` text,
-  `ativo` int(1) DEFAULT '1'
+  `titulo` varchar(50) DEFAULT NULL,
+  `fk_idempresa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `tbempresa`
+-- Extraindo dados da tabela `tberros_tipo`
 --
 
-INSERT INTO `tbempresa` (`id`, `nome`, `cliente_code`, `logo_code`, `missao`, `visao`, `valores`, `ativo`) VALUES
-(1, 'SGT - Gestão e Tecnologia', 'sgtgestaoetecnologia', NULL, 'Proporcionar ferramentas que facilitem a gestão das práticas e excelência organizacional.', NULL, 'Respeito ao usuário.\r\nRapidez nas soluções.\r\nInovação permantente.', 1);
+INSERT INTO `tberros_tipo` (`id`, `titulo`, `fk_idempresa`) VALUES
+(1, 'Leve', 1),
+(2, 'Intermediário', 1),
+(3, 'Grave', 1);
 
 -- --------------------------------------------------------
 
@@ -162,10 +238,9 @@ INSERT INTO `tbempresa` (`id`, `nome`, `cliente_code`, `logo_code`, `missao`, `v
 CREATE TABLE `tberros` (
   `id` int(11) NOT NULL,
   `titulo` varchar(255) DEFAULT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
-  `fk_idempresa` int(11) DEFAULT NULL
+  `fk_idempresa` int(11) DEFAULT NULL,
+  `fk_idtipo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -182,20 +257,21 @@ CREATE TABLE `tberros_documentos` (
   `fk_idetapa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbetapa`
+-- Estrutura da tabela `tbcompetencias`
 --
 
-CREATE TABLE `tbetapa` (
+CREATE TABLE `tbcompetencias` (
   `id` int(11) NOT NULL,
-  `titulo` varchar(255) DEFAULT NULL,
-  `ativo` int(11) DEFAULT '1',
+  `tipo` varchar(12) DEFAULT NULL,
+  `fk_iddocumento` int(11) DEFAULT NULL,
+  `fk_idetapa` int(11) DEFAULT NULL,
+  `fk_idusuario` int(11) DEFAULT NULL,
+  `fk_idcargo` int(11) DEFAULT NULL,
   `fk_idempresa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -227,42 +303,6 @@ CREATE TABLE `tbferias_func` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbgrupo`
---
-
-CREATE TABLE `tbgrupo` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(255) DEFAULT NULL,
-  `fk_idempresa` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tbhorario_trab`
---
-
-CREATE TABLE `tbhorario_trab` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(255) DEFAULT NULL,
-  `manha_entrada` time DEFAULT NULL,
-  `manha_saida` time DEFAULT NULL,
-  `tarde_entrada` time DEFAULT NULL,
-  `tarde_saida` time DEFAULT NULL,
-  `fk_idempresa` int(11) DEFAULT NULL,
-  `ativo` int(11) DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `tbhorario_trab`
---
-
-INSERT INTO `tbhorario_trab` (`id`, `titulo`, `manha_entrada`, `manha_saida`, `tarde_entrada`, `tarde_saida`, `fk_idempresa`, `ativo`) VALUES
-(1, 'Regular', '07:00:00', '12:00:00', '13:00:00', '18:00:00', 1, 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `tblog_documentos`
 --
 
@@ -290,7 +330,6 @@ CREATE TABLE `tblog_documentos_tempo` (
   `data_fim` datetime DEFAULT NULL,
   `total_minutos` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -346,32 +385,6 @@ CREATE TABLE `tbtimer` (
   `timestamp` int(10) UNSIGNED DEFAULT NULL,
   `fk_idusuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tbusuario`
---
-
-CREATE TABLE `tbusuario` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `usuario` varchar(45) DEFAULT NULL,
-  `senha` varchar(100) DEFAULT NULL,
-  `ativo` int(11) DEFAULT '1',
-  `fk_idempresa` int(11) NOT NULL,
-  `fk_idcargos` int(11) NOT NULL,
-  `fk_idhorariotrab` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `tbusuario`
---
-
-INSERT INTO `tbusuario` (`id`, `nome`, `email`, `usuario`, `senha`, `ativo`, `fk_idempresa`, `fk_idcargos`, `fk_idhorariotrab`) VALUES
-(1, 'Administrador', 'suporte@sgtgestaoetecnologia.com.br', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 1, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -438,7 +451,8 @@ ALTER TABLE `tbempresa`
 -- Indexes for table `tberros`
 --
 ALTER TABLE `tberros`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tipo` (`fk_idtipo`);
 
 --
 -- Indexes for table `tberros_documentos`
@@ -449,6 +463,13 @@ ALTER TABLE `tberros_documentos`
   ADD KEY `fk_tberros_documentos_tbusuario1_idx` (`fk_idusuario`),
   ADD KEY `fk_tberros_has_tbdocumentos_cad_tberros1_idx` (`fk_iderros`),
   ADD KEY `fk_tberros_documentos_tbetapa` (`fk_idetapa`);
+
+--
+-- Indexes for table `tberros_tipo`
+--
+ALTER TABLE `tberros_tipo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbempresa` (`fk_idempresa`);
 
 --
 -- Indexes for table `tbetapa`
@@ -509,9 +530,9 @@ ALTER TABLE `tblog_sistema`
 --
 ALTER TABLE `tbobservacoes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_idusuario` (`fk_idusuario`),
-  ADD KEY `fk_idetapa` (`fk_idetapa`),
-  ADD KEY `fk_idprotocolo` (`fk_idprotocolo`);
+  ADD KEY `tbobservacoes_ibfk_1` (`fk_idusuario`),
+  ADD KEY `tbobservacoes_ibfk_2` (`fk_idetapa`),
+  ADD KEY `tbobservacoes_ibfk_3` (`fk_idprotocolo`);
 
 --
 -- Indexes for table `tbprazoetapa`
@@ -582,6 +603,11 @@ ALTER TABLE `tbempresa`
 -- AUTO_INCREMENT for table `tberros`
 --
 ALTER TABLE `tberros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tberros_tipo`
+--
+ALTER TABLE `tberros_tipo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `tbetapa`
@@ -682,6 +708,12 @@ ALTER TABLE `tbdocumentos_cad`
   ADD CONSTRAINT `fk_tbdocumentos_cad_tbdocumento1` FOREIGN KEY (`fk_iddocumento`) REFERENCES `tbdocumento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Limitadores para a tabela `tberros`
+--
+ALTER TABLE `tberros`
+  ADD CONSTRAINT `fk_tipo` FOREIGN KEY (`fk_idtipo`) REFERENCES `tberros_tipo` (`id`);
+
+--
 -- Limitadores para a tabela `tberros_documentos`
 --
 ALTER TABLE `tberros_documentos`
@@ -691,10 +723,16 @@ ALTER TABLE `tberros_documentos`
   ADD CONSTRAINT `fk_tberros_has_tbdocumentos_cad_tberros1` FOREIGN KEY (`fk_iderros`) REFERENCES `tberros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Limitadores para a tabela `tberros_tipo`
+--
+ALTER TABLE `tberros_tipo`
+  ADD CONSTRAINT `fk_tbempresa` FOREIGN KEY (`fk_idempresa`) REFERENCES `tbempresa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Limitadores para a tabela `tbetapa`
 --
 ALTER TABLE `tbetapa`
-  ADD CONSTRAINT `tbetapa_ibfk_1` FOREIGN KEY (`fk_idempresa`) REFERENCES `tbempresa` (`id`);
+  ADD CONSTRAINT `tbetapa_ibfk_1` FOREIGN KEY (`fk_idempresa`) REFERENCES `tbempresa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tbferiados`
@@ -713,7 +751,7 @@ ALTER TABLE `tbferias_func`
 -- Limitadores para a tabela `tbgrupo`
 --
 ALTER TABLE `tbgrupo`
-  ADD CONSTRAINT `tbgrupo_ibfk_1` FOREIGN KEY (`fk_idempresa`) REFERENCES `tbempresa` (`id`);
+  ADD CONSTRAINT `tbgrupo_ibfk_1` FOREIGN KEY (`fk_idempresa`) REFERENCES `tbempresa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tbhorario_trab`
@@ -725,24 +763,24 @@ ALTER TABLE `tbhorario_trab`
 -- Limitadores para a tabela `tbobservacoes`
 --
 ALTER TABLE `tbobservacoes`
-  ADD CONSTRAINT `tbobservacoes_ibfk_1` FOREIGN KEY (`fk_idusuario`) REFERENCES `tbusuario` (`id`),
-  ADD CONSTRAINT `tbobservacoes_ibfk_2` FOREIGN KEY (`fk_idetapa`) REFERENCES `tbetapa` (`id`),
-  ADD CONSTRAINT `tbobservacoes_ibfk_3` FOREIGN KEY (`fk_idprotocolo`) REFERENCES `tbdocumentos_cad` (`id`);
+  ADD CONSTRAINT `tbobservacoes_ibfk_1` FOREIGN KEY (`fk_idusuario`) REFERENCES `tbusuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbobservacoes_ibfk_2` FOREIGN KEY (`fk_idetapa`) REFERENCES `tbetapa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbobservacoes_ibfk_3` FOREIGN KEY (`fk_idprotocolo`) REFERENCES `tbdocumentos_cad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tbprazoetapa`
 --
 ALTER TABLE `tbprazoetapa`
-  ADD CONSTRAINT `tbprazoetapa_ibfk_1` FOREIGN KEY (`fk_iddocumento`) REFERENCES `tbdocumentos_cad` (`id`),
-  ADD CONSTRAINT `tbprazoetapa_ibfk_2` FOREIGN KEY (`fk_idetapas`) REFERENCES `tbetapa` (`id`);
+  ADD CONSTRAINT `tbprazoetapa_ibfk_1` FOREIGN KEY (`fk_iddocumento`) REFERENCES `tbdocumentos_cad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbprazoetapa_ibfk_2` FOREIGN KEY (`fk_idetapas`) REFERENCES `tbetapa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tbtimer`
 --
 ALTER TABLE `tbtimer`
-  ADD CONSTRAINT `tbtimer_ibfk_1` FOREIGN KEY (`fk_iddoccad`) REFERENCES `tbdocumentos_cad` (`id`),
-  ADD CONSTRAINT `tbtimer_ibfk_2` FOREIGN KEY (`fk_idetapa`) REFERENCES `tbetapa` (`id`),
-  ADD CONSTRAINT `tbtimer_ibfk_3` FOREIGN KEY (`fk_idusuario`) REFERENCES `tbusuario` (`id`);
+  ADD CONSTRAINT `tbtimer_ibfk_1` FOREIGN KEY (`fk_iddoccad`) REFERENCES `tbdocumentos_cad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbtimer_ibfk_2` FOREIGN KEY (`fk_idetapa`) REFERENCES `tbetapa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbtimer_ibfk_3` FOREIGN KEY (`fk_idusuario`) REFERENCES `tbusuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tbusuario`
